812a51abd9683a9eef1d36f70e69e2201370f7df
siddique <siddiquemd161@gmail.com>
2025-08-25 16:01:58 +0600

    cash out done

--- home.js
+++ home.js
@@ -1,38 +1,43 @@
 // home.js
-const buttonAddMoey=document.getElementById("btn-add-money")
+const buttonAddMoney=document.getElementById("btn-add-money")
 const acountNumber=document.getElementById("Ac-Number")
 const addAmount=document.getElementById("Amount")
 const pinNumber=document.getElementById("pin")
 const totalBalance=document.getElementById("balance")
+const buttonCashOut=document.getElementById("btn-Cash-Out")
 
 
 function addMoneyConvert(id){
+   
+   
    const value=id.value
    const valueConvert= parseInt(value)
    return valueConvert
     
 }
+// Add amount
 
-
-
-
-
-
-buttonAddMoey.addEventListener("click",function(even){
+buttonAddMoney.addEventListener("click",function(even){
     even.preventDefault()
     const money=addMoneyConvert(addAmount)
     
    const totalAmount=totalBalance.innerText
       const amount=parseInt(totalAmount)
-   
-   
-   
+   const total=money+amount
 
-    const total=money+amount
+totalBalance.innerText=total    
+})
 
-totalBalance.innerText=total
-   
+// cash out
+buttonCashOut.addEventListener("click",function(even){
+    even.preventDefault()
+    console.log("first")
+    const withdraw=document.getElementById("Amount-withdraw")
+    const money=addMoneyConvert(withdraw)
     
-    
-    
+   const totalAmount=totalBalance.innerText
+      const amount=parseInt(totalAmount)
+   const total=amount-money
+
+totalBalance.innerText=total    
 })
